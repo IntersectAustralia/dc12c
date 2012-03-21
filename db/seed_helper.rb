ca922b410eb4fc2d45ecb5c19c0b2ca65379c4ea
Ryan Braganza <ryan.braganza@gmail.com>
2012-03-21 14:36:38 +1100

    Add Languages, Genres, Countries to seed data

--- db/seed_helper.rb
+++ db/seed_helper.rb
@@ -1,6 +1,13 @@
 def create_roles_and_permissions
   Permission.delete_all
   Role.delete_all
+  Language.delete_all
+  Country.delete_all
+  Genre.delete_all
+
+  create_countries %w{Greece Italy Australia}
+  create_languages %w{Greek Latin English Demotic}
+  create_genres %w{Book Fragment Scrap Painting}
 
   #TODO: create your roles here
   superuser = "Administrator"
@@ -27,3 +34,20 @@ def create_permission(entity, action, roles)
   end
 end
 
+def create_countries country_names
+  country_names.each do |name|
+    Country.create! name: name
+  end
+end
+
+def create_languages language_names
+  language_names.each do |name|
+    Language.create! name: name
+  end
+end
+
+def create_genres genre_names
+  genre_names.each do |name|
+    Genre.create! name: name
+  end
+end
