257821d92a77d4b55ce57495c68e63bdfacd4018
Ryan Braganza <ryan.braganza@gmail.com>
2012-06-06 14:56:44 +1000

    remove commented-out code

--- app/controllers/user_registers_controller.rb
+++ app/controllers/user_registers_controller.rb
@@ -61,7 +61,6 @@ class UserRegistersController < Devise::RegistrationsController
     if resource.update_password(params[resource_name])
       set_flash_message :notice, :password_updated if is_navigational_format?
       sign_in resource_name, resource, :bypass => true
-      #respond_with resource, :location => after_update_path_for(resource)
       redirect_to :users_profile
     else
       clean_up_passwords(resource)
