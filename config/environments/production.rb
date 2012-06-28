3e4b1136c7c2de6b71a76f655ab608b021a806ac
Ryan Braganza <ryan.braganza@gmail.com>
2012-06-28 11:41:50 +1000

    DC12C-46 add tracking code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,5 +68,6 @@ Dc12c::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  GA.tracker = "UA-xxxxxx-x" # FIXME
+
+  GA.tracker = "UA-16667092-17"
 end
