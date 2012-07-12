d2b0bcdb05b791c93293b0f7b31dd7096d09dceb
Ryan Braganza <ryan.braganza@gmail.com>
2012-07-12 17:59:34 +1000

    DC12C-26 add spatial/temporal coverage to model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120627013334) do
+ActiveRecord::Schema.define(:version => 20120712073204) do
 
   create_table "access_requests", :force => true do |t|
     t.integer  "user_id",        :precision => 38, :scale => 0
@@ -23,10 +23,12 @@ ActiveRecord::Schema.define(:version => 20120627013334) do
 
   create_table "collections", :force => true do |t|
     t.string   "title"
-    t.string   "description", :limit => 512
+    t.string   "description",       :limit => 512
     t.string   "keywords"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
+    t.string   "spatial_coverage"
+    t.string   "temporal_coverage"
   end
 
   create_table "collections_papyri", :id => false, :force => true do |t|
